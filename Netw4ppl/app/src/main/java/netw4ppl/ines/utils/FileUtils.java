cf6d97acf80d389b1c14df3b6c9297a03ad81cfb
consithe1 <constantin.thebaudeau@hotmail.com>
2021-04-26 17:13:28 +0200

    CreateUtils(String path)

--- Netw4ppl/app/src/main/java/netw4ppl/ines/utils/FileUtils.java
+++ Netw4ppl/app/src/main/java/netw4ppl/ines/utils/FileUtils.java
@@ -1,4 +1,22 @@
 package netw4ppl.ines.utils;
 
 public class FileUtils {
+
+    /**
+     * Create an empty file
+     *
+     * @param path path for the two files
+     * @return a boolean to determine whether the file was created successfully or not
+     */
+    public static boolean createFile(String path) {
+        Log.d("general-display", "I need to create this file : " + path);
+        File file = new File(path);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file.exists();
+    }
+
 }
