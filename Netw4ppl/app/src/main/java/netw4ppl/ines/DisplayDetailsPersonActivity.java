ce5df5f100f3740d455ce085c6d3c6299f6f2416
consithe1 <constantin.thebaudeau@hotmail.com>
2021-04-28 11:34:18 +0200

    Modification visuel DetailsPerson

--- Netw4ppl/app/src/main/java/netw4ppl/ines/DisplayDetailsPersonActivity.java
+++ Netw4ppl/app/src/main/java/netw4ppl/ines/DisplayDetailsPersonActivity.java
@@ -3,12 +3,28 @@ package netw4ppl.ines;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class DisplayDetailsPersonActivity extends AppCompatActivity {
+
+    TextView mTextViewFullnameTitle;
+    Button mButtonAddRelationFrom;
+    Button mButtonEditPerson;
+    Button mButtonDeletePerson;
+    Button mButtonAddRelationTo;
+
+    ListView mListRelationsFrom;
+    ListView mDetailsPerson;
+    ListView mListRelationsTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_details_person);
+
+
+
     }
 }
