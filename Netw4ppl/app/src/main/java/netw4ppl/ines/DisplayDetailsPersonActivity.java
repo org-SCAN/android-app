69253981cfdf89e87af54ab187fee87580dbfba8
consithe1 <constantin.thebaudeau@hotmail.com>
2021-05-07 09:03:18 +0200

    EditPerson
    Redirige désormais vers AddPerson mais avec les champs pré-remplis (les champs de texte uniquement pour le moment)

--- Netw4ppl/app/src/main/java/netw4ppl/ines/DisplayDetailsPersonActivity.java
+++ Netw4ppl/app/src/main/java/netw4ppl/ines/DisplayDetailsPersonActivity.java
@@ -2,6 +2,7 @@ package netw4ppl.ines;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
@@ -40,13 +41,22 @@ public class DisplayDetailsPersonActivity extends AppCompatActivity {
         mButtonEditPerson = (Button) findViewById(R.id.display_person_edit);
         mButtonDeletePerson = (Button) findViewById(R.id.display_person_delete);
         
-        mListDetailsPerson = (ListView) findViewById(R.id.list_details_person); 
+        mListDetailsPerson = (ListView) findViewById(R.id.list_details_person);
+
+        Person person = ManagePersonsActivity.array_persons.get(index_person);
+
+        mTextViewFullnameTitle.setText(person.getInfoByKey("full_name"));
+
+        PersonDetailsListAdapter adapter_details_person = new PersonDetailsListAdapter(this, R.layout.adapter_details_person_fields, MainActivity.mConfiguration.getArrayFields());
+        mListDetailsPerson.setAdapter(adapter_details_person);
 
         mButtonAddRelationFrom.setOnClickListener(v -> {
 
         });
         mButtonEditPerson.setOnClickListener(v -> {
-
+            AddPersonActivity.person = person;
+            Intent intent = new Intent(DisplayDetailsPersonActivity.this, AddPersonActivity.class);
+            startActivity(intent);
         });
         mButtonDeletePerson.setOnClickListener(v-> {
 
@@ -54,14 +64,5 @@ public class DisplayDetailsPersonActivity extends AppCompatActivity {
         mButtonAddRelationTo.setOnClickListener(v -> {
 
         });
-
-        if (index_person != -1) {
-            Person person = ManagePersonsActivity.array_persons.get(index_person);
-
-            mTextViewFullnameTitle.setText(person.getInfoByKey("full_name"));
-
-            PersonDetailsListAdapter adapter_details_person = new PersonDetailsListAdapter(this, R.layout.adapter_details_person_fields, MainActivity.mConfiguration.getArrayFields());
-            mListDetailsPerson.setAdapter(adapter_details_person);
-        }
     }
 }
