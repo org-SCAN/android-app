a5799b2cbb1542b228d1929b09a4641092a88adc
Dvd-Luc <Dvd-Luc>
2021-05-10 18:19:11 +0200

    Mise en place des dropDowns et du spinner

--- Netw4ppl/app/src/main/java/netw4ppl/ines/AddRelationActivity.java
+++ Netw4ppl/app/src/main/java/netw4ppl/ines/AddRelationActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 
 import org.json.JSONException;
 
+import netw4ppl.ines.utils.DataElement;
 import netw4ppl.ines.utils.Person;
 import netw4ppl.ines.utils.PersonListAdapter;
 import netw4ppl.ines.utils.Relation;
@@ -22,11 +24,6 @@ public class AddRelationActivity extends AppCompatActivity {
 
     private static final String TAG = "AddRelationActivity";
     public static Relation single_relation;
-
-    TextView mTextViewRelationFrom;
-    TextView mTextViewRelationType;
-    TextView mTextViewRelationTo;
-    TextView mTextViewRelationComment;
 
     AutoCompleteTextView mAutoTextViewRelationFrom;
     AutoCompleteTextView mAutoTextViewRelationTo;
@@ -67,11 +64,29 @@ public class AddRelationActivity extends AppCompatActivity {
 
     private void generateViews() {
         mAutoTextViewRelationFrom = findViewById(R.id.add_relation_from);
-        ArrayAdapter<Person> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, ManagePersonsActivity.array_persons);
-        mAutoTextViewRelationFrom.setAdapter(adapter);
+        mAutoTextViewRelationTo = findViewById(R.id.add_relation_to);
+
+        ArrayAdapter<Person> autocomplete_adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, ManagePersonsActivity.array_persons);
+
+        mAutoTextViewRelationFrom.setAdapter(autocomplete_adapter);
+        mAutoTextViewRelationTo.setAdapter(autocomplete_adapter);
+
+        mSpinnerRelationType = findViewById(R.id.add_relation_type);
+        ArrayAdapter<DataElement> spinner_adapter = MainActivity.mConfiguration.getArrayAdapter("relations");
+        mSpinnerRelationType.setAdapter(spinner_adapter);
+
+
+        mEditTextRelationComments = findViewById(R.id.add_relation_comments);
     }
 
     private void getRelationInputsFromViews() {
-
+        final Person[] tmp_from = new Person[1];
+        mAutoTextViewRelationFrom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                tmp_from[0] = (Person) mAutoTextViewRelationFrom.getAdapter().getItem(position);
+            }
+        });
+        Log.d(TAG,tmp_from[0].toString());
     }
 }
