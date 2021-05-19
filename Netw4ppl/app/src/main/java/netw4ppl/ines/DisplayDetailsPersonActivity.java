22adaa165b252dec824956a1c5c84f562c162c5a
consithe1 <constantin.thebaudeau@hotmail.com>
2021-05-19 16:29:36 +0200

    Triptique relations
    Ajout des relations associées dans les litsview associées

--- Netw4ppl/app/src/main/java/netw4ppl/ines/DisplayDetailsPersonActivity.java
+++ Netw4ppl/app/src/main/java/netw4ppl/ines/DisplayDetailsPersonActivity.java
@@ -11,11 +11,15 @@ import android.widget.TextView;
 
 import org.json.JSONException;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import netw4ppl.ines.utils.FileUtils;
 import netw4ppl.ines.utils.Person;
 import netw4ppl.ines.utils.PersonDetailsListAdapter;
+import netw4ppl.ines.utils.PersonDetailsRelationFromAdapter;
+import netw4ppl.ines.utils.PersonDetailsRelationToAdapter;
+import netw4ppl.ines.utils.Relation;
 
 public class DisplayDetailsPersonActivity extends AppCompatActivity {
 
@@ -26,6 +30,8 @@ public class DisplayDetailsPersonActivity extends AppCompatActivity {
     Button mButtonAddRelationTo;
 
     PersonDetailsListAdapter adapter_details_person;
+    PersonDetailsRelationFromAdapter adapter_relations_from;
+    PersonDetailsRelationToAdapter adapter_relations_to;
 
     ListView mListRelationsFrom;
     ListView mListDetailsPerson;
@@ -46,13 +52,19 @@ public class DisplayDetailsPersonActivity extends AppCompatActivity {
         mButtonDeletePerson = (Button) findViewById(R.id.display_person_delete);
         
         mListDetailsPerson = (ListView) findViewById(R.id.list_details_person);
+        mListRelationsFrom = (ListView) findViewById(R.id.list_relations_from);
+        mListRelationsTo = (ListView) findViewById(R.id.list_relations_to);
 
         Person person = ManagePersonsActivity.array_persons.get(index_person);
 
         mTextViewFullnameTitle.setText(person.getInfoByKey("full_name"));
 
+        /* Adapters for the different list views */
         adapter_details_person = new PersonDetailsListAdapter(this, R.layout.adapter_details_person_fields, MainActivity.mConfiguration.getArrayFields());
         mListDetailsPerson.setAdapter(adapter_details_person);
+
+        updateAdapterFrom();
+        updateAdapterTo();
 
         mButtonAddRelationFrom.setOnClickListener(v -> {
             AddRelationActivity.setToPerson(person);
@@ -103,10 +115,52 @@ public class DisplayDetailsPersonActivity extends AppCompatActivity {
         });
     }
 
+    private ArrayList<Relation> getRelationsFrom(Person to_person) {
+        ArrayList<Relation> array_relations_from = new ArrayList<Relation>();
+
+        for (int i=0; i<ManageRelationsActivity.array_relations.size(); i++) {
+            Relation r = ManageRelationsActivity.array_relations.get(i);
+            if (r.getTo().equals(to_person.toString())) {
+                array_relations_from.add(r);
+            }
+        }
+
+        return array_relations_from;
+    }
+
+    private void updateAdapterFrom() {
+        Person person = ManagePersonsActivity.array_persons.get(index_person);
+        ArrayList<Relation> array_relations_from = getRelationsFrom(person);
+        adapter_relations_from = new PersonDetailsRelationFromAdapter(this, R.layout.adapter_relation_details_person, array_relations_from);
+        mListRelationsFrom.setAdapter(adapter_relations_from);
+    }
+
+    private void updateAdapterTo() {
+        Person person = ManagePersonsActivity.array_persons.get(index_person);
+        ArrayList<Relation> array_relations_to = getRelationsTo(person);
+        adapter_relations_to = new PersonDetailsRelationToAdapter(this, R.layout.adapter_relation_details_person, array_relations_to);
+        mListRelationsTo.setAdapter(adapter_relations_to);
+    }
+
+    private ArrayList<Relation> getRelationsTo(Person from_person) {
+        ArrayList<Relation> array_relations_to = new ArrayList<Relation>();
+
+        for (int i=0; i<ManageRelationsActivity.array_relations.size(); i++) {
+            Relation r = ManageRelationsActivity.array_relations.get(i);
+            if (r.getFrom().equals(from_person.toString())) {
+                array_relations_to.add(r);
+            }
+        }
+
+        return array_relations_to;
+    }
+
     @Override
     protected void onResume () {
         super.onResume();
         adapter_details_person.notifyDataSetChanged();
+        updateAdapterFrom();
+        updateAdapterTo();
     }
 
     public boolean deleteAssociatedRelations(Person p) {
