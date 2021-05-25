cc3badf942717a12b8ab9646974279a8570cae82
consithe1 <constantin.thebaudeau@hotmail.com>
2021-05-25 09:56:30 +0200

    Update ManageRelationsActivity.java
    Après recherche, on associe l'index réel

--- Netw4ppl/app/src/main/java/netw4ppl/ines/ManageRelationsActivity.java
+++ Netw4ppl/app/src/main/java/netw4ppl/ines/ManageRelationsActivity.java
@@ -91,10 +91,23 @@ public class ManageRelationsActivity extends AppCompatActivity {
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+                boolean got_it = false;
+                int index_reel = position;
+                int i = 0;
+                Relation r = (Relation) adapter.getItemAtPosition(position);
+
+                while (i < array_relations.size() && !got_it) {
+                    if (array_relations.get(i).equals(r)) {
+                        index_reel = i;
+                        got_it = true;
+                    }
+                    i++;
+                }
+
                 Intent intent = new Intent(ManageRelationsActivity.this, DisplayDetailsRelationActivity.class);
 
                 Bundle b = new Bundle();
-                b.putInt("index_relation", position); // your index relation
+                b.putInt("index_relation", index_reel); // your index relation
                 intent.putExtras(b); //Put your id to your next Intent
 
                 startActivity(intent);
