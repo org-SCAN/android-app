0be8d356b1483841e77dada87088866cfe3ee230
Dvd-Luc <Dvd-Luc>
2021-06-04 11:17:01 +0200

    Partial doc of Relation.java

--- Netw4ppl/app/src/main/java/netw4ppl/ines/utils/Relation.java
+++ Netw4ppl/app/src/main/java/netw4ppl/ines/utils/Relation.java
@@ -19,6 +19,10 @@ public class Relation extends JSONObject {
     private String from_person_name;
     private String to_person_name;
 
+    /**
+     * Default constructor of Relation
+     * Sets from_person_name_ and to_person_name to empty Strings
+     */
     public Relation(){
         super();
 
@@ -26,6 +30,12 @@ public class Relation extends JSONObject {
         to_person_name = "";
     }
 
+    /**
+     * Constructor of Relation
+     * Sets the informations of a Relation
+     * @param infos_relat String containing the informations of a relation in a JSON syntax
+     * @throws JSONException
+     */
     public Relation(String infos_relat) throws JSONException {
         super(infos_relat);
 
@@ -33,45 +43,86 @@ public class Relation extends JSONObject {
         to_person_name = "";
     }
 
+    /**
+     * Sets the fullname of the from person
+     * @param full_name String of the from person's fullname
+     */
     public void setFromFullname(String full_name) {
         from_person_name = full_name;
     }
 
+    /**
+     * Sets the fullname of the to person
+     * @param full_name String of the to person's fullname
+     */
     public void setToFullname(String full_name) {
         to_person_name = full_name;
     }
 
+    /**
+     * Sets the relation type using the key of a relation type (e.g SE for service)
+     * @param key_relation String of the key of a relation type
+     */
     public void setRelationType(String key_relation) {
         this.putInfo("relation", key_relation);
     }
 
+    /**
+     * Sets the to informations of a relation using a Person
+     * @param person to Person used to set the relation
+     */
     public void setPersonTo(Person person) {
         this.putInfo("to_unique_id", person.getInfoByKey("unique_id"));
         to_person_name = person.getInfoByKey("full_name");
     }
 
+    /**
+     * Sets the to informations of a relation using a String
+     * @param unique_id String containing the unique_id of the to Person
+     */
     public void setPersonTo(String unique_id) {
         this.putInfo("to_unique_id", unique_id);
     }
 
+    /**
+     * Sets the from informations of a relation using a Person
+     * @param person from Person used to set the relation
+     */
     public void setPersonFrom(Person person) {
         this.putInfo("from_unique_id", person.getInfoByKey("unique_id"));
         from_person_name = person.getInfoByKey("full_name");
     }
 
+    /**
+     * Sets the from informations of a relation using a String
+     * @param unique_id String containing the unique_id of the from Person
+     */
     public void setPersonFrom(String unique_id) {
         this.putInfo("from_unique_id", unique_id);
     }
 
+    /**
+     * Sets the details of a Relations using a String
+     * @param details details to be added to the Relation
+     */
     public void setDetails(String details) {
         this.putInfo("detail", details);
     }
 
+    /**
+     * Sets the Unique ID of a Relation
+     */
     public void setUUIDRelation() {
         String uuid = UUID.randomUUID().toString();
         this.putInfo("relation_application_id", uuid);
     }
 
+    /**
+     * Adds an information to the Relation with the format "key:value"
+     *
+     * @param key String containing the key of the information to be added
+     * @param info String containing the value of the information to be added
+     */
     public void putInfo(String key, String info) {
         try {
             this.put(key, info);
@@ -80,6 +131,11 @@ public class Relation extends JSONObject {
         }
     }
 
+    /**
+     * Returns a String corresponding to an information's value associated to a key
+     * @param key String of the key of the information we look for
+     * @return String containing the value associated to the key or an empty String if there was no associated value
+     */
     public String getInfoByKey(String key) {
         String res = "";
         try {
@@ -90,6 +146,11 @@ public class Relation extends JSONObject {
         return res;
     }
 
+    /**
+     * Tests the equality of the Relation we use with another one
+     * @param relat Relation to be tested for equality
+     * @return a boolean, true if the relations are equal, false if they are different
+     */
     public boolean isSameRelation(Relation relat) {
         boolean res_uid_from = false;
         boolean res_uid_to = false;
@@ -100,6 +161,11 @@ public class Relation extends JSONObject {
         return res_uid_from && res_uid_to && res_type_relation;
     }
 
+    /**
+     * Tests if a Person is already from or to of the Relation we use
+     * @param p Person to be tested
+     * @return a boolean telling if the Person is already contained in the Relation, true if yes, false if not
+     */
     public boolean isPersonInRelation(Person p) {
         String p_id = p.getInfoByKey("unique_id");
         String p_full_name = p.getInfoByKey("full_name");
